aba35e41b6a4b55f8115cffa6a88009d71509e0b
camilleabetong <cabetong@calpoly.edu>
2024-11-07 10:04:39 -0800

    Final Commit

--- command_line.py
+++ command_line.py
@@ -1,1 +1,10 @@
+import sys
+from convert import str_to_float
 
+if __name__ == "__main__":
+    total = 0.0
+    for arg in sys.argv[1:]:
+        number = str_to_float(arg)
+        if number is not None:
+            total += number
+    print("Sum:", total)
--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,4 @@
+from convert import str_to_float
 
+print(str_to_float("12")) #Expected: 12.0
+print(str_to_float("camille abetong")) #Expected: none
